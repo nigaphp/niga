0d721d404cd59806dc62d528847b4071e2507498
Abass Ben Cheik <cheikabassben@gmail.com>
2021-07-13 11:17:44 +0000

    Update public/index.php

--- public/index.php
+++ public/index.php
@@ -1,14 +1,37 @@
 <?php
+/**
+* This file is part of the Nigatedev PHP framework 
+* 
+* (c) Abass Ben Cheik <abass@todaysdev.com>
+*/
 
-require_once "../vendor/autoload.php";
+define('APP_ROOT_DIR', dirname(__DIR__));
 
-use Nigatedev\Application;
+require_once APP_ROOT_DIR. "/vendor/autoload.php";
 
-$app = new Application();
+use Nigatedev\Application as App;
 
-$app->router->get("/", function(){
-  
+/**
+* Create a new Application from the main core application package
+* @var Nigatedev\Core\Application $app
+*/
+$app = new App();
+
+/**
+* @return render route "/" E.g: http://localhost:8000/ and output Home to the client
+*/
+$app->router->get("/", function() {
   return "<h1>Home</h1>";
 });
 
+/**
+* @return render route "/contact" E.g: http://localhost:8000/contact and output Contact to the client
+*/
+$app->router->get("/contact", function() {
+  return "<h1>Contact</h1>";
+});
+
+/**
+* @return Run request uri / Throw HTTP Not Found Exception from Nigatedev\Core\Request
+*/
 $app->run();
