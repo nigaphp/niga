59ac2cfda3098a1f4dffa9d560c65d72262ba7c5
Abass Ben Cheik <cheikabassben@gmail.com>
2021-07-18 21:52:31 +0000

    Update public/index.php

--- public/index.php
+++ public/index.php
@@ -1,20 +1,24 @@
 <?php
 /**
- * This file is part of the Nigatedev PHP framework 
+ * This file is part of the Nigatedev PHP framework package
  * 
  * (c) Abass Ben Cheik <abass@todaysdev.com>
  */
+ 
+namespace Nigatedev\Controller;
 
 require_once dirname(__DIR__). "/vendor/autoload.php";
 
 use Nigatedev\Trunk;
 use Nigatedev\Core\App;
-use Nigatedev\Controller\HomeController;
+use Nigatedev\Maker\Make;
+
+var_dump(new Make());
 /**
-* Create a new Application from the main core application package
- *
-* @var Nigatedev\Core\Application $app
-*/
+ * We recommend using the command line interface to generate route / controller
+ * E.g: this command [bin/controller make:controller HomeController] 
+ * will generate you a valid route that point to [/home] route and a controller class into src/Controller directory
+ */
 $app = new App(dirname(__DIR__));
 
 /**
