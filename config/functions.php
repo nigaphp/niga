ee6563e5877963dc0a8b78ef1ac8fb1c03c717d8
Abass Ben Cheik <cheikabassben@gmail.com>
2021-08-02 08:34:47 +0000

    [Feat] add config/functions.php

---
+++ config/functions.php
@@ -0,0 +1,14 @@
+<?php
+if(!function_exists("dump") && !is_file(dirname(__DIR__)."/vendor/nigatedev/dumper/src/functions.php")){
+  function dump(){
+    echo "<div><span style='color:#ff000b'>Fatal error:</span> 
+    Can't use [nigatedev/dumper] because it is not installed, 
+    try <kbd>composer update</kbd>
+    or use default <kbd>var_dump</kbd></div>";
+  
+    
+  }
+    return;
+}
+
+require_once dirname(__DIR__)."/vendor/nigatedev/dumper/src/functions.php";
