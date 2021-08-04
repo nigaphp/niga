93b7960b9abcf5d4c8a39a7a19432570e81c1bf8
Abass Ben Cheik <cheikabassben@gmail.com>
2021-08-04 16:37:42 +0000

    [Feat] Config PHPUnit and enable debug mode

--- public/index.php
+++ public/index.php
@@ -1,12 +1,26 @@
 <?php
+declare(strict_types=1);
+
 /**
  * This file is part of the Nigatedev PHP framework package
  *
  * (c) Abass Ben Cheik <abass@todaysdev.com>
  */
 
+use Nigatedev\Core\App;
+use Nigatedev\Core\Debugger\Debugger;
+
 require_once dirname(__DIR__)."/vendor/autoload.php";
 
-$app = new Nigatedev\Core\App(dirname(__DIR__));
-$app->router->load(dirname(__DIR__)."/config/loader.php");
+if (file_exists(dirname(__DIR__)."/.env")) {
+
+ (Dotenv\Dotenv::createImmutable(dirname(__DIR__)))->load();
+ 
+ if(isset($_ENV["DEBUG_MODE"]) && $_ENV["DEBUG_MODE"] == true){
+    Debugger::enableDebugMode();
+ }
+}
+
+$app = new App(dirname(__DIR__));
+$app->router->load(App::$APP_ROOT."/config/loader.php");
 $app->run();
