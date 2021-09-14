0f6af2237118cabeb736ea770a3497df2791ac68
Abass Ben Cheik <cheikabassben@gmail.com>
2021-09-14 23:59:16 +0000

    refactor: change doctrine orm config chema

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
+use Nigatedev\FrameworkBundle\Application\Configuration;
 use Nigatedev\FrameworkBundle\Config\ORMConfig;
 use Nigatedev\Framework\Parser\JSONParser;
 use Nigatedev\Framework\Parser\Exception\ParseException;
@@ -7,7 +8,7 @@ use Nigatedev\Framework\Parser\Exception\ParseException;
 require_once "./config/env.loader.php";
 
 try {
-    $getOrmConfig = JSONParser::parseJFile(__DIR__, "/config/dependencies/doctrine.json");
+    $getOrmConfig = JSONParser::parseJFile(Configuration::getAppRoot(), "/config/dependencies/doctrine.json");
 } catch (ParseException $e) {
     die($e->getMessage() . " In file " . $e->getFile() . " On line " . $e->getLine());
 }
