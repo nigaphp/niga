f6e712336d77095209d977bfd6e412a8e0c03ee3
Abass Ben Cheik <cheikabassben@gmail.com>
2021-09-23 22:43:11 +0000

    refactor(clean): change slashes(\) to php magic constant DIRECTORY_SEPARATOR

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types = 1);
 
@@ -19,8 +19,9 @@ use Nigatedev\FrameworkBundle\Config\Loader;
 use Nigatedev\Framework\Support\File;
 
 define("ROOT_DIR", dirname(__DIR__));
+define("DSP", DIRECTORY_SEPARATOR);
 
-$autoLoader = ROOT_DIR."/vendor/autoload.php";
+$autoLoader = ROOT_DIR.DSP."vendor".DSP."autoload.php";
 
 if (!file_exists($autoLoader)) {
     throw new RuntimeException('Install dependencies with "composer install" to run this script.');
@@ -28,7 +29,7 @@ if (!file_exists($autoLoader)) {
 
 require_once($autoLoader);
 
-if (File::isFile(ROOT_DIR."/.env")) {
+if (File::isFile(ROOT_DIR.DSP.".env")) {
     (Dotenv\Dotenv::createImmutable(ROOT_DIR))->load();
  
     if (isset($_ENV["DEBUG_MODE"]) && (bool)$_ENV["DEBUG_MODE"] === true) {
