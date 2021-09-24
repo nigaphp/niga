ba4e4afed49a4d02c37bba42a37a26504d63f3d6
Abass Ben Cheik <cheikabassben@gmail.com>
2021-09-24 03:37:06 +0000

    docs: cleaning

--- public/index.php
+++ public/index.php
@@ -2,15 +2,6 @@
 
 declare(strict_types = 1);
 
-/*
- * This file is part of the Nigatedev PHP framework package
- *
- * (c) Abass Ben Cheik <abass@todaysdev.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 use App\Trunk;
 use Nigatedev\FrameworkBundle\Application\App;
 use Nigatedev\FrameworkBundle\Debugger\Debugger;
