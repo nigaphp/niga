ab1e57118aa7c4b14f537f9c65625383bf482998
Abass Ben Cheik <cheikabassben@gmail.com>
2021-09-25 23:23:35 +0000

    style(views): clean base view

--- views/base.php
+++ views/base.php
@@ -3,13 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../build/app.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="http://localhost:8000/build/app.css" type="text/css" media="all" />
     <link rel="icon" type="image/png" href="../images/nigatedev.icon.png" sizes="96x96">
     <title>{{title}}</title>
   </head>
   <body>
-    <div class="be-container">
        {{body}}
-    </div>
   </body>
 </html>
