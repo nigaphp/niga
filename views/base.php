d2e14e2605f3f455d0f990ef8be1e714f2fb099b
Abass Ben Cheik <cheikabassben@gmail.com>
2021-09-27 22:56:20 +0000

    style(views): clean base views

--- views/base.php
+++ views/base.php
@@ -1,13 +1,12 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?= self::styleAsset('app') ?>" type="text/css" media="all" />
-    <link rel="icon" type="image/png" href="<?= self::imageAsset('nigatedev.icon.png') ?>" sizes="96x96">
-    <title>{{title}}</title>
-  </head>
-  <body>
-       {{body}}
-  </body>
+    <title>Site title</title>
+</head>
+<body>
+    {{body}}
+</body>
 </html>
