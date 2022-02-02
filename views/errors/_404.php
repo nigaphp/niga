862c646dfbcfdcfbb82023b8ae0ae80a9b6b702d
Abass Ben Cheick <cheikabassben@gmail.com>
2022-02-02 20:52:50 +0000

    update: 404 template model

--- views/errors/_404.php
+++ views/errors/_404.php
@@ -1,8 +1,8 @@
 <?php if ($this->getDebug() === false): ?>
 <div class="container">
   <h1 class="text-danger">Oops 🙄 404 Page Not Found!</h1>
-  <p>Sorry this page is not found</code></p>
-  <small>Create a route for your <a href="/">home page?</a> <code>bin/niga make:make-home-page</code></small>
+  <p>Sorry this page is not found on this server.</code></p>
+  <small>Create a route for your <a href="/home">home page?</a> <code>bin/niga make:make HomeController</code></small>
 </div>
 <?php else: ?>
 <div class="container">
@@ -12,6 +12,6 @@
     </div>
     <p class="text-danger">ERROR: <code>404 Page Not Found!</code></p>
       You are seeing this because the debug mode is enabled.
-    <p>You can use the following command <span><code>bin/niga make:make-home-page</code></span> to create a route for your / home page</p>
+    <p>You can use the following command <span><code>bin/niga make:make HomeController</code></span> to create a route for your /home page</p>
 </div>
 <?php endif ?>
