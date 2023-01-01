ec3eecdd77958465b7a1d39aec8d70df89ce3528
abass-dev <abassthedev@gmail.com>
2023-01-01 20:42:04 +0000

    fix: assets path

--- views/base.php
+++ views/base.php
@@ -1,12 +1,16 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?= self::styleAsset('app') ?>" type="text/css" media="all" />
-    <title>Site title</title>
+    <title>Nigatedev Framework</title>
 </head>
+
 <body>
+
     {{body}}
 </body>
+
 </html>
